b212a53603e77516ac847431b1e7cd1d5cb64817
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-04 15:22:53 +0100

    creato array di oggetti in VUE

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            todoList : [
+                {
+                    text : 'Fare la spesa',
+                    done : false
+                },
+                {
+                    text : 'Comprare regali di natale',
+                    done : false
+                },
+                {
+                    text : 'Prenotare viaggio',
+                    done : true
+                },
+            ]
 
         }
     },
